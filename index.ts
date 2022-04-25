3a4ad72e27451850158ceabf0f38bf428ff63302
Filipos <83700574+pidizman@users.noreply.github.com>
2022-04-25 19:03:09 +0000

    some edits in index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("hello!");
